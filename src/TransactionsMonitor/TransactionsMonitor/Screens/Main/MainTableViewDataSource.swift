290bad5a4269523b0778f5f477e564aedfc60164
iMamad <mohammad.farrahi@oculid.com>
2023-01-20 19:27:51 +0100

    displayed data on cells in from MainTableViewDataSource.

--- src/TransactionsMonitor/TransactionsMonitor/Screens/Main/MainTableViewDataSource.swift
+++ src/TransactionsMonitor/TransactionsMonitor/Screens/Main/MainTableViewDataSource.swift
@@ -8,13 +8,28 @@
 import UIKit
 
 class MainTableViewDataSource: NSObject, UITableViewDataSource {
+    
+    private var cellID = "cell"
+    private var _transactions: [PBTransaction]?
+    
+    var transactions: [PBTransaction]? {
+        get {
+            _transactions
+        }
+        set {
+            _transactions = newValue
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        _transactions?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "row \(indexPath.row)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        if let companyName = _transactions?[indexPath.row].companyName {
+            cell.textLabel?.text = companyName
+        }
         return cell
     }
 }
