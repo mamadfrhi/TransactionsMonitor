8e4a6a49f4158a9b8d4520fa7475f0dab4646cc3
iMamad <mohammad.farrahi@oculid.com>
2023-01-21 00:20:13 +0100

    added services to MainVM.

--- src/TransactionsMonitor/TransactionsMonitor/Screens/Main/MainVM.swift
+++ src/TransactionsMonitor/TransactionsMonitor/Screens/Main/MainVM.swift
@@ -10,6 +10,10 @@ import Foundation
 class MainVM {
     
     // MARK: Dependecies
+    private let services = TransactionServices(transactionsAPI:
+                                                TransactionsAPI(session:
+                                                                    TransactionsAPISessionManager(with: "Token").getTransactionsSession(),
+                                                                localJSONLoader: LocalJSONLoader()))
     
     // MARK: Obserbables
     @Published var error: String?
