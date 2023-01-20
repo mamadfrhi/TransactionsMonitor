5baf56ab9fb4928bfd719364e8d619f8ac264f14
iMamad <mohammad.farrahi@oculid.com>
2023-01-20 19:04:58 +0100

    added MainVM

---
+++ src/TransactionsMonitor/TransactionsMonitor/Screens/Main/MainVM.swift
@@ -0,0 +1,19 @@
+//
+//  MainVM.swift
+//  TransactionsMonitor
+//
+//  Created by Mohammad Farrahi on 20.01.23.
+//
+
+import Foundation
+
+class MainVM {
+    
+    // MARK: Dependecies
+    
+    // MARK: Obserbables
+    @Published var error: String?
+    @Published var transactions: [PBTransaction] = []
+    
+    
+}
