a7b4c4bb02823be082029d05437c22e57d3e312d
iMamad <mohammad.farrahi@oculid.com>
2023-01-20 15:05:49 +0100

    initialized xcode project.

---
+++ src/TransactionsMonitor/TransactionsMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransactionsMonitor
+//
+//  Created by Mohammad Farrahi on 20.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
