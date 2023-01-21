a3edf4f8281682a39e455561d0d3e191379b96f8
iMamad <mohammad.farrahi@oculid.com>
2023-01-21 15:15:09 +0100

    updated DateConverter.

--- src/TransactionsMonitor/TransactionsMonitor/Resources/DateConverter.swift
+++ src/TransactionsMonitor/TransactionsMonitor/Resources/DateConverter.swift
@@ -9,11 +9,11 @@ import Foundation
 
 struct DateConverter {
     
-    func readableDate(from inputDate: String) -> String {
+    func readablestringDate(from ISO8601Date: String) -> String {
         // iso
-        let isoString = inputDate
+        let isoString = ISO8601Date
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         if let isoDate = formatter.date(from: isoString) {
             // normal
             formatter.dateFormat = "yyyy-MM-dd"
@@ -22,6 +22,11 @@ struct DateConverter {
         }
         return formatter.string(from: Date())
         // if data conversion failed, return today's date
+    }
+    
+    func readableDate(from ISO8601Date: String) -> Date {
+        ISO8601DateFormatter().date(from: ISO8601Date) ?? Date()
+        // if date conversion failed, return today's date
     }
     
 }
