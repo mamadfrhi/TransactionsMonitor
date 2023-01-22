358b913cb0accd82d40e98b87f182e1328cbf118
iMamad <mohammad.farrahi@oculid.com>
2023-01-22 11:57:40 +0100

    added Settings screen.

---
+++ src/TransactionsMonitor/TransactionsMonitor/Screens/Settings/SettingsVC.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsVC.swift
+//  TransactionsMonitor
+//
+//  Created by Mohammad Farrahi on 22.01.23.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    
+    //MARK: Factory
+    class func `init`() -> SettingsVC {
+        let id = "SettingsView"
+        let storyboard = UIStoryboard(name: id, bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: id) as! SettingsVC
+        return vc
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Settings"
+    }
+
+}
