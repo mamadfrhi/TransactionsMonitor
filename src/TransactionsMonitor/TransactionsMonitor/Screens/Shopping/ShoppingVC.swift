41e834056fb7225785927c07bf6c4c5a605c9b43
iMamad <mohammad.farrahi@oculid.com>
2023-01-22 10:14:53 +0100

    added Shopping screen.

---
+++ src/TransactionsMonitor/TransactionsMonitor/Screens/Shopping/ShoppingVC.swift
@@ -0,0 +1,29 @@
+//
+//  ShoppingVC.swift
+//  TransactionsMonitor
+//
+//  Created by Mohammad Farrahi on 22.01.23.
+//
+
+import UIKit
+
+class ShoppingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
